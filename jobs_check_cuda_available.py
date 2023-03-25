2fc3a3280425f3c7435f04a2b5708cb7a53cb3d5
hanming <hanming@anyscale.com>
2023-03-25 01:59:13 +0000

    add print logs

--- jobs_check_cuda_available.py
+++ jobs_check_cuda_available.py
@@ -6,13 +6,18 @@ driver script.
 import ray
 import torch
 
-ray.init()
-
 # For good measure, let's check that we can use the GPU
 # in a remote function.
 @ray.remote(num_gpus=0.1)
 def foo():
   return torch.cuda.is_available()
 
+print("Starting Check Cuda Available Job")
+ray.init(address="auto")
 # Assert that GPU resources are available in the driver script
-assert(ray.get(foo.remote()))
+result = ray.get(foo.remote())
+
+assert result, "CUDA is not available in the driver script"
+
+print("Check Cuda Available Job Succeeded")
+
