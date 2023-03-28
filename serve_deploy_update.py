037b02da915f7d490e78df03555a3fde829d77fa
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-03-28 07:53:28 -0700

    Add ability to kill head node.

--- serve_deploy_update.py
+++ serve_deploy_update.py
@@ -1,9 +1,25 @@
 import asyncio
-
-from ray import serve
+import logging
+import subprocess
 from typing import Any, Dict
 
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+
+import ray
+from ray import serve
+
+app = FastAPI()
+
+logger = logging.getLogger("ray.serve")
+
+@ray.remote(num_gpus=1)
+def stop_ray_on_head_node():
+    return subprocess.check_output("ray stop -f", shell=True)
+
+
 @serve.deployment(user_config={"response": "Hello world!", "should_fail": False})
+@serve.ingress(app)
 class Updatable:
     def __init__(self):
         self._response: str = ""
@@ -15,9 +31,18 @@ class Updatable:
 
     def check_health(self):
         if self._should_fail:
+            logger.info("Failing health check intentionally.")
             raise Exception("Intentionally failing.")
 
-    async def __call__(self, *args) -> str:
-        return self._response
+    @app.get("/")
+    def respond(self) -> PlainTextResponse:
+        logger.info(f"Responding with '{self._response}'.")
+        return PlainTextResponse(self._response)
+
+    @app.post("/kill-head-node")
+    async def kill(self) -> PlainTextResponse:
+        logger.info(f"Killing head node.")
+        await stop_ray_on_head_node.remote()
+        return PlainTextResponse("ok")
 
 bound = Updatable.bind()
