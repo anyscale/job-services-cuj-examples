13196dd37e16624c0736a678815ae5ba818330f1
brucez-anyscale <brucez@anyscale.com>
2023-07-27 13:10:03 -0700

    Add a python file for job

---
+++ run_simple_gpu_job.py
@@ -0,0 +1,13 @@
+import time
+import ray
+
+@ray.remote(num_cpus=4)
+def foo():
+  count = 1
+  while count < 10:
+    print("A loop...")
+    time.sleep(10)
+    count += 1
+  return True
+
+assert(ray.get(foo.remote()))
