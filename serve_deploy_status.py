76d14c22ca0a3e6f7d96e79ba1fbb0c0beface25
Shreyas Krishnaswamy <shrekris@anyscale.com>
2024-01-31 13:21:54 -0800

    Take in a version
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- serve_deploy_status.py
+++ serve_deploy_status.py
@@ -1,11 +1,21 @@
 from ray import serve
 from ray.serve.schema import ServeStatus
 
+from typing import Dict
+
 @serve.deployment
 class GetStatus:
 
+    def __init__(self, version: str):
+        self.version = version
+
     def __call__(self, *args) -> ServeStatus:
         status = serve.status()
-        return status
+        return {
+            "status": status,
+            "version": self.version,
+        }
 
-app = GetStatus.bind()
+
+def build_app(args: Dict[str, str]):
+    return GetStatus.bind(args["version"])
